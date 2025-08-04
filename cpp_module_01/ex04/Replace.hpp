11a53cbef23ed70e06b3bc090aebddbadceeca1c
Daruuu <thedaruny16@gmail.com>
2025-08-04 23:01:00 +0200

    add structure of ex04

---
+++ cpp_module_01/ex04/Replace.hpp
@@ -0,0 +1,18 @@
+#ifndef REPLACE_HPP
+#define REPLACE_HPP
+#include <string>
+
+class Replace
+{
+private:
+	std::string	filename_;
+	std::string	stringOne_;
+	std::string	stringTwo_;
+
+public:
+	Replace();
+	~Replace();
+
+};
+
+#endif //REPLACE_HPP
