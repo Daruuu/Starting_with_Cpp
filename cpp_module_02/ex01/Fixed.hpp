3dbdd0543faa2d355faa83df7801fc11f850da32
Daruuu <thedaruny16@gmail.com>
2025-08-08 17:30:24 +0200

    add ex 01

---
+++ cpp_module_02/ex01/Fixed.hpp
@@ -0,0 +1,33 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#define	RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+
+class Fixed
+{
+
+private:
+	int					fixedPointValue_;
+	static const int	fractionalBits_ = 8;
+
+public:
+	Fixed();
+	Fixed(const int intValue);
+	Fixed(const float intValue);
+	Fixed(const Fixed &other);
+	Fixed &operator =(const Fixed& other);
+	~Fixed();
+
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+	float	toFloat( void ) const;
+	int		toInt( void ) const;
+};
+#endif //FIXED_HPP
