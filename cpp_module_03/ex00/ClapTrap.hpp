87048e09e447fba5cfba8263b24770da393279fc
Daruuu <thedaruny16@gmail.com>
2025-08-12 17:41:45 +0200

    add structure of module cpp03 ex00

---
+++ cpp_module_03/ex00/ClapTrap.hpp
@@ -0,0 +1,17 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+#include <string>
+
+class ClapTrap
+{
+private:
+	std::string name_;
+	int	hitPoints_;
+	int energyPoints_;
+
+
+public:
+
+};
+
+#endif //CLAPTRAP_HPP
