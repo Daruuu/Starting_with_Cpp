b2f472c6a1774543ab571068e795cb1edc93c721
Daruuu <thedaruny16@gmail.com>
2025-08-21 15:30:49 +0200

    create IMateriaSource and MateriaSource implementing IMateriaSource

---
+++ cpp_module_04/ex03/MateriaSource.hpp
@@ -0,0 +1,25 @@
+#ifndef MATERIASOURCE_HPP
+#define MATERIASOURCE_HPP
+
+#include "IMateriaSource.hpp"
+
+class MateriaSource : public IMateriaSource
+{
+public:
+	void learnMateria(AMateria*);
+	// createMateria(std::string const &);
+
+};
+
+/*
+learnMateria(AMateria*)
+Copies the Materia passed as a parameter and stores it in memory so it can be cloned
+later. Like the Character, the MateriaSource can know at most 4 Materias. They
+are not necessarily unique.
+• createMateria(std::string const &)
+Returns a new Materia. The latter is a copy of the Materia previously learned by
+the MateriaSource whose type equals the one passed as parameter. Returns 0 if
+the type is unknown.
+ */
+
+#endif //MATERIASOURCE_HPP
